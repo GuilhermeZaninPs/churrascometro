3ef7669752580af117818c99113c8b5e43933743
unknown <guizanin224@gmail.com>
2021-02-25 18:29:25 +0000

    JavaScript

--- script/script.js
+++ script/script.js
@@ -16,70 +16,70 @@ let resultado = document.getElementById("resultado");
 // Carne para Homens
 function carnePH(duracao){
     if(duracao >= 6){
-        return 600
+        return 600;
     } else{
-        return 400
+        return 400;
     }
-}
+};
 
 // Carne para Mulheres
 function carnePM(duracao){
     if(duracao >= 6){
-        return 500
+        return 500;
     } else{
-        return 300
+        return 300;
     }
-}
+};
 
 // Carne para Crianças
 function carnePC(duracao){
     if(duracao >= 6){
-        return 400
+        return 400;
     } else{
-        return 200
+        return 200;
     }
-}
+};
 
 // Bebida para Homens
 function bebidaPH(duracao){
     if(duracao >= 6){
-        return 2000
+        return 2000;
     } else{
-        return 1200
+        return 1200;
     }
-}
+};
 
 // Bebida para Mulheres
 function bebidaPM(duracao){
     if(duracao >= 6){
-        return 1800
+        return 1800;
     } else{
-        return 1000
+        return 1000;
     }
-}
+};
 
 // Bebida para Crianças
 function bebidaPC(duracao){
     if(duracao >= 6){
-        return 1200
+        return 1200;
     } else{
-        return 600
+        return 600;
     }
-}
+};
 
 
 //Calcular
 function calcular(){
-    let homens = carneTotalHomem.value
-    let mulheres = carneTotalMulher.value
-    let crianca = carneTotalCrianca.value
-    let duracao = totalDuracao.value
+    let homens = carneTotalHomem.value;
+    let mulheres = carneTotalMulher.value;
+    let crianca = carneTotalCrianca.value;
+    let duracao = totalDuracao.value;
 
-    let totalCarne = homens * carnePH(duracao) + mulheres * carnePM(duracao) + crianca * carnePC(duracao)
-    let totalBebida = homens * bebidaPH(duracao) + mulheres * bebidaPM(duracao) + crianca * bebidaPC(duracao)
+    let totalCarne = homens * carnePH(duracao) + mulheres * carnePM(duracao) + crianca * carnePC(duracao);
+    let totalBebida = homens * bebidaPH(duracao) + mulheres * bebidaPM(duracao) + crianca * bebidaPC(duracao);
     
-    resultado.innerHTML = `<h1>Você precisará de: </h1>`
-    resultado.innerHTML += `<p>${(totalCarne / 1000)}Kg de Carne</p>`
-    resultado.innerHTML += `<p>${Math.ceil(totalBebida / 1000)}L de Bebida</p>`
+    resultado.innerHTML = `<h1>Você precisará de: </h1>`;
+    resultado.innerHTML += `<p>${(totalCarne / 1000)}Kg de Carne</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(totalBebida / 1000)}L de Bebida</p>`;
     
-}
+};
